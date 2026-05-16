536ddf95bbcd9ee8ff2b6e8aca47adf1c2898b99
Jorge Bolívar Blandino <jorge.boliblandi@alum.uca.es>
2026-05-16 11:11:14 +0200

    Añadido campo guia y rutas por _id

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-const ReservaSchema = new mongoose.Schema({
+const reservaSchema = new mongoose.Schema({
     nickname: String,
     carnet: Number,
     telefono: String,
-    email: { type: String, required: true }, // Nuestro ID
+    email: { type: String, required: true },
     fecha: String,
-    hora: String
+    hora: String,
+    guia: String // <-- NUEVO CAMPO AGREGADO
 }, {
-    // Esto evita que Mongoose añada un campo __v (versión) que no necesitamos en Android
     versionKey: false 
 });
 
-module.exports = mongoose.model('Reserva', ReservaSchema);
+module.exports = mongoose.model('Reserva', reservaSchema);
